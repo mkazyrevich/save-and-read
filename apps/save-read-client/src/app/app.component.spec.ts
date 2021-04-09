50ca22308282eb7cd38796197fa5897d5ff9a543
kazyrevich <mkozyrevich@gmail.com>
2021-04-10 01:53:38 +0300

    init commit
    Took 2 hours 57 minutes

--- apps/save-read-client/src/app/app.component.spec.ts
+++ apps/save-read-client/src/app/app.component.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -24,8 +25,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain(
-      'Welcome to save-read-client!'
-    );
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to save-read-client!');
   });
 });
